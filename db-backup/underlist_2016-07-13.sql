d56f6af61abfa3c1d5b50a8d26cdf826c6538ff7
Juan Carlos Toledo Stávoli <jcarlostoledos@gmail.com>
2016-07-13 03:33:23 -0500

    Updated db / added api example
    Postman example

---
+++ db-backup/underlist_2016-07-13.sql
@@ -0,0 +1,139 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.6.16)
+# Database: underlist
+# Generation Time: 2016-07-13 08:32:14 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table Lists
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Lists`;
+
+CREATE TABLE `Lists` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `description` text,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `lists_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `Lists` WRITE;
+/*!40000 ALTER TABLE `Lists` DISABLE KEYS */;
+
+INSERT INTO `Lists` (`id`, `title`, `user_id`, `description`, `created_date`)
+VALUES
+	(3,'titulo',1,'esta es una descripcion','2000-01-01 00:00:00');
+
+/*!40000 ALTER TABLE `Lists` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table Tasks
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Tasks`;
+
+CREATE TABLE `Tasks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `created_date` datetime DEFAULT NULL,
+  `due_date` datetime DEFAULT NULL,
+  `done` bit(1) DEFAULT NULL,
+  `list_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `list_id` (`list_id`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`list_id`) REFERENCES `Lists` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `Tasks` WRITE;
+/*!40000 ALTER TABLE `Tasks` DISABLE KEYS */;
+
+INSERT INTO `Tasks` (`id`, `title`, `description`, `created_date`, `due_date`, `done`, `list_id`)
+VALUES
+	(39,'titulo','esta es una descripcion','2000-01-01 00:00:00','2000-01-01 00:00:00',b'0',3),
+	(40,'titulo','esta es una descripcion','2000-01-01 00:00:00','2000-01-01 00:00:00',b'0',3),
+	(42,'titulo','esta es una descripcion','2000-01-01 00:00:00','2000-01-01 00:00:00',b'0',3);
+
+/*!40000 ALTER TABLE `Tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table Users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Users`;
+
+CREATE TABLE `Users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+
+INSERT INTO `Users` (`id`, `username`, `name`)
+VALUES
+	(1,'admin','admin');
+
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table UserTasks
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `UserTasks`;
+
+CREATE TABLE `UserTasks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `task_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `task_id` (`task_id`),
+  CONSTRAINT `usertasks_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `Tasks` (`id`),
+  CONSTRAINT `usertasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `UserTasks` WRITE;
+/*!40000 ALTER TABLE `UserTasks` DISABLE KEYS */;
+
+INSERT INTO `UserTasks` (`id`, `user_id`, `task_id`)
+VALUES
+	(26,1,39),
+	(27,1,40),
+	(28,1,42);
+
+/*!40000 ALTER TABLE `UserTasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
